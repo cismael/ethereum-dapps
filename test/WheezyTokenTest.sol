97a014095f3f80261f008fcae30ec9a12177e080
Ismaël COULIBALY <c.ismael@live.fr>
2020-12-28 22:41:25 +0100

    Adding unit test

---
+++ test/WheezyTokenTest.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "truffle/Assert.sol";
+import "../contracts/WheezyToken.sol";
+
+contract WheezyTokenTest {
+
+    WheezyToken wheezyToken;
+
+    constructor() public {
+        wheezyToken  = new WheezyToken();
+    }
+
+    // Testing the adopt() function
+    function testTotalSupply() public {
+        uint returned = wheezyToken.totalSupply();
+        uint expected = 1000000;
+
+        Assert.equal(returned, expected, "Total Supply should be 1000000.");
+    }
+
+    function testTransferFrom() public  {
+        address _to = 0x89EAB984AbB3E7Cc0f847dc321fCD9B95a538f05;
+
+        bool result = wheezyToken.transfer(_to, 1);
+
+        Assert.isTrue(result, "Transfer should succeed.");
+    }
+
+}
